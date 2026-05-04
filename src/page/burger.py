71bb35e0ed9b0bff6455b606bba36196806451ec
alcidescadiz <alcides.cadiz@gmail.com>
2026-05-03 20:59:56 -0400

    update 03-05-2026

--- src/page/burger.py
+++ src/page/burger.py
@@ -11,85 +11,83 @@ def burger_page(page,cambiar_pagina):
             "img": "img/burger/doble_smash.jpg",
             "title": "Burger doble Smash Cheese",
             "description": " Hamburguesa con doble carne de res, doble feta de queso cheddar, pepinillos, salsa especial de la casa, presentada en un delicioso pan de papa y acompañadas de una ración de papas.",
-            "price": "Precio 12.00"
+            "price": "Precio 12.10"
         },
         {
             "img": "img/burger/burger_de_carne.jpg",
             "title": "Burger de Carne",
             "description": "Hamburguesa de carne vacuna al grill en pan de papa, con lechuga, cebolla, tomate y salsa especial Kevin.",
-            "price": "Precio 5.60",
+            "price": "Precio 5.70",
             "adicional": True
         },
         {
             "img": "img/burger/burger_de_pollo_crispy.jpg",
             "title": "Chicken Crispy Burger",
             "description": "Pollo empanizado en pan de papa con salsa miel mostaza, lechuga, tomate y cebolla.",
-            "price": "Precio 8.50",
+            "price": "Precio 8.60",
             "adicional": True
         },
         {
             "img": "img/burger/burger_pollo_cesar.jpg",
             "title": "Burger Pollo César",
             "description": "Hamburguesa de pollo con aderezo César casero, lechuga fresca, tocineta crujiente y queso parmesano en pan brioche.",
-            "price": "Precio 11.60"
+            "price": "Precio 11.70"
         },
         {
             "img": "img/burger/tnt_burger.jpg",
             "title": "Tnt Burger",
             "description": "Carne de res, tocineta, tomates confitados, queso crema empanizado, salsa de pimentón ahumado.",
-            "price": "Precio 10.00"
+            "price": "Precio 10.10"
         },
         {
             "img": "img/burger/burger_ribs.jpg",
             "title": "Rib's Burger",
             "description": "Carne de res y cerdo con salsa BBQ, quesos amarillo y mozzarella, aros de cebolla crujientes y tocineta caramelizada en pan brioche.",
-            "price": "Precio 13.00"
+            "price": "Precio 13.10"
+            ""
         },
         {
             "img": "img/burger/burger_hillary.jpg",
             "title": "Burger Hillary",
             "description": "Filete de pollo empanizado, queso crema empanizado, tocineta, lechuga y salsa César en pan tostado, acompañada de papas fritas crujientes.",
-            "price": "Precio 11.00"
+            "price": "Precio 11.10"
         },
         {
             "img": "img/burger/smoked_burger.jpg",
             "title": "Smoked Burger",
             "description": "Blend ahumado (Carne de res y cerdo), feta de queso cheddar, cebolla caramelizada en pan de ceniza de berenjena o brioche, acompañadas de una ración de papas.",
-            "price": "Precio 11.40"
+            "price": "Precio 11.50"
         },
         {
             "img": "img/burger/febrero2026.jpg",
             "title": "Burger de temporada Febrero 2026",
             "description": "Blend ahumado (Carne de res y cerdo), crema de champiñones con queso azul, tocineta y mozzarella en un pan de parmesano.",
-            "price": "Precio 12.80"
+            "price": "Precio 12.90"
         },
         {
             "img": "img/burger/trioburger.jpg",
             "title": "Medium Trio Burger",
             "description": "Degustación de tres hamburguesas: Res Smashed, Pollo Crispy y Costilla. Incluye tocineta, vegetales, salsas artesanales (Kevin y Mostaza Miel) y un aro de cebolla crujiente. Acompañadas de papas fritas y dip de ketchup. ",
-            "price": "Precio 15.00"
+            "price": "Precio 15.10"
         },
         {
             "img": "img/burger/burger_italy.jpg",
             "title": "Burger Italy",
             "description": "Disfruta de una jugosa carne de res a la parrilla, acompañada de queso mozzarella fundido, galleta de parmesano, tomate confitado, rucula y nuestra vibrante salsa pesto.",
-            "price": "Precio 10.00"
+            "price": "Precio 10.10"
         },
         {
             "img": "img/burger/burger_de_costilla.jpg",
             "title": "Burger Costilla BBQ",
             "description": "Hamburguesa de costilla de cerdo en pan de papa con salsa BBQ casera, lechuga, tomate y cebolla.",
-            "price": "Precio 8.60",
+            "price": "Precio 8.70",
             "adicional": True
-        }
-    ]
-
-    viernes_a_domingos = [
+        },
         {
             "img": "img/burger/burger_ribs_chicken.jpg",
             "title": "Burger Ribs & Chicken Supreme",
             "description": "Carne de cerdo con salsa BBQ, pollo empanizado, quesos amarillo y mozzarella, aros de cebolla crujientes y tocineta caramelizada en pan brioche.",
-            "price": "Precio 13.00"
+            "price": "Precio 13.10"
         },
         {
             "img": "img/burger/smokedprime.jpg",
@@ -101,14 +99,18 @@ def burger_page(page,cambiar_pagina):
             "img": "img/burger/burger_emmy.jpg",
             "title": "Burger Emmy Chicken",
             "description": "La Salsa Emmy, a base de pasta coreana Gochujang, que tiene un picor suave y textura cremosa, acompaña un Pollo empanizado en Doritos, tocineta caramelizada con BBQ, queso cheddar fundido, lechuga fresca y pepinillos, acompañada de nuestras crujientes papas fritas. ",
-            "price": "Precio 11.50"
+            "price": "Precio 11.60"
         }
     ]
-    hoy = datetime.date.today().weekday()
 
-    if hoy == 4 or hoy == 5 or hoy == 6:
-        # burger + viernes_a_domingos
-        burger = burger + viernes_a_domingos
+    # viernes_a_domingos = [
+        
+    # ]
+    # hoy = datetime.date.today().weekday()
+
+    # if hoy == 4 or hoy == 5 or hoy == 6:
+    #     # burger + viernes_a_domingos
+    #     burger = burger + viernes_a_domingos
 
     return ft.Column(
         controls=[
